f76ec99bc48b9b2878895605239b079daee8b636
gary.archer <gary.archer@curity.io>
2025-02-18 16:37:10 +0100

    Updates from review comments

--- chapter-12-platform-specific-apps/desktop-app/src/renderer/apiRequest.ts
+++ chapter-12-platform-specific-apps/desktop-app/src/renderer/apiRequest.ts
@@ -1,12 +1,38 @@
+/*
+ *  Copyright 2024 Curity AB
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 import {ApplicationError} from '../shared/applicationError';
 import {IpcEventNames} from '../shared/ipcEventNames';
 import {ApiOptions} from './apiOptions';
 import {ipcRequest} from './ipcRequest';
+import {Order} from '../shared/order';
+import {UserInfo} from '../shared/userInfo';
+
+export async function getOrders(): Promise<Order[]> {
+    return await makeApiRequest({name: IpcEventNames.ApiOrders}) as Order[];
+}
+
+export async function getUserInfo(): Promise<UserInfo> {
+    return await makeApiRequest({name: IpcEventNames.ApiOrders}) as UserInfo;
+}
 
 /*
- * A generic method in the React app to make reliable API that handle token refresh and expiry
+ * The React app uses a generic method to make reliable API calls that handle token refresh and expiry
  */
-export async function makeApiRequest(options: ApiOptions): Promise<any> {
+async function makeApiRequest(options: ApiOptions): Promise<any> {
         
     try {
 
@@ -32,7 +58,7 @@ export async function makeApiRequest(options: ApiOptions): Promise<any> {
                 throw e2;
             }
 
-            // Indicate a login is required if refresh fails with an invalid_grant error
+            // Indicate a login is required if token refresh fails with an invalid_grant error
             throw new ApplicationError('login_required', 'User must re-authenticate');
         }
 
