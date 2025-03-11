c7016f73a4314e209eb717c2ec47578d6280fb78
gary.archer <gary.archer@curity.io>
2025-03-11 12:19:53 +0100

    License integration updates

--- resources/authorizationserver/download-license.sh
+++ resources/authorizationserver/download-license.sh
@@ -57,6 +57,15 @@ function getLicenseToolDownloadFileName() {
 }
 
 #
+# First check that the jq tool, used to read the license file, is installed
+#
+jq --version 1>/dev/null
+if [ $? -ne 0 ]; then
+  echo 'Please install the jq tool'
+  exit 1
+fi
+
+#
 # Do nothing if there is already a valid license file on disk
 #
 if [ $(needsLicenseDownload) == 0 ]; then
@@ -64,16 +73,7 @@ if [ $(needsLicenseDownload) == 0 ]; then
 fi
 
 #
-# First check that the jq tool, used to read the license file, is installed
-#
-jq --version 2>/dev/null
-if [ $? -ne 0 ]; then
-  echo 'Please install the jq tool'
-  exit 1
-fi
-
-#
-# Inform the user, who can use their development or work email to get a community edition license
+# Inform the user who will see a browser window, but not every time they run a deployment
 #
 echo 'This script gets a community edition license for the Curity Identity Server.'
 echo 'A CLI will run a code flow in the system browser to get an access token with which to download the license.'
@@ -86,35 +86,38 @@ read -n 1
 DOWNLOAD_FILENAME="$(getLicenseToolDownloadFileName)"
 if [ ! -f $DOWNLOAD_FILENAME ]; then
 
-  curl -s -L -O "https://github.com/curityio/curity-license-tool/releases/1.0.0/$DOWNLOAD_FILENAME"
-  if [ $? -ne 0 ]; then
-    echo 'Problem encountered downloading the license file utility'
-    exit 1
-  fi
+  echo 'copying'
+  cp ../../../book-license-cli/curity-book-cli .
+  
+  #curl -s -L -O "https://github.com/curityio/curity-license-tool/releases/1.0.0/$DOWNLOAD_FILENAME"
+  #if [ $? -ne 0 ]; then
+  #  echo 'Problem encountered downloading the license file utility'
+  #  exit 1
+  #fi
 fi
 
 #
 # Unzip the executable
 #
-tar -zxf $DOWNLOAD_FILENAME
-if [ $? -ne 0 ]; then
-  echo '*** Problem encountered unpacking the license tool'
-  exit 1
-fi
+#tar -zxf $DOWNLOAD_FILENAME
+#if [ $? -ne 0 ]; then
+#  echo '*** Problem encountered unpacking the license tool'
+#  exit 1
+#fi
 
 #
-# Execute the license tool to run a code flow and give it the folder to which the tool should save the license file
+# Execute the license tool to run a code flow and wait for the response
 # 
-./curity-license-tool "$(pwd)"
+./curity-book-cli
 if [ $? -ne 0 ]; then
-  echo 'Problem encountered running a code flow with the license tool'
+  echo 'Problem encountered running a code flow with the Curity license tool'
   exit 1
 fi
 
 #
-# Make a final check for success, to handle conditions like cancellation
+# Check that there is now a valid license on disk
 #
-if [ $(needsLicenseDownload) == 1 ]; then
+if [ ! -f "$LICENSE_FILE_PATH" ]; then
   echo 'The license file download did not complete successfully'
   exit 1
 fi
